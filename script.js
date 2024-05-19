dc03bbd0e7bd6b9217b6f33cd9d2293a353d4166
copyright111 <nathinwankr@gmail.com>
2024-05-19 11:12:04 +0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('loader-overlay').style.display = 'flex';
-setTimeout(function () {
-    document.getElementById('loader-overlay').style.display = 'none';
-
-    // Display the website content after 3 seconds
-    document.getElementById('website-content').style.display = 'block';
-}, 3000);
